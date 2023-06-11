56a77c842b8c4ae3e49092b08ce0d31b14ed064e
vijayrakkaiah <vijay.rakkaiah@gmail.com>
2023-06-11 16:58:39 +0530

    string convert

--- add.py
+++ add.py
@@ -1,8 +1,3 @@
-word = 'ABCdEfg'
-
-for i in range(len(word)):
-    if word[i]>='A' and word[i]<='Z':
-        print(chr(ord(word[i])+32), end='')
-
-    else:
-        print(word[i], end = '')
+a = 10
+b = 15
+print (a+b)
