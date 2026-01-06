c888a6dbff3cb7c5c445af4fef271b0e0c696656
Vijay Rakkaiah <136189222+VijayRakkaiah@users.noreply.github.com>
2026-01-06 23:07:28 +0530

    Add files via upload

---
+++ bitwise_operator_overloading.py
@@ -0,0 +1,72 @@
+# Python program to demonstrate
+# operator overloading
+
+
+class BitwiseOperatorOverloading():
+    def __init__(self, value):
+        self.value = value
+
+    def __and__(self, obj):
+        print("And operator overloaded")
+        if isinstance(obj, Geek):
+            return self.value & obj.value
+        else:
+            raise ValueError("Must be a object of class Geek")
+
+    def __or__(self, obj):
+        print("Or operator overloaded")
+        if isinstance(obj, Geek):
+            return self.value | obj.value
+        else:
+            raise ValueError("Must be a object of class Geek")
+
+    def __xor__(self, obj):
+        print("Xor operator overloaded")
+        if isinstance(obj, Geek):
+            return self.value ^ obj.value
+        else:
+            raise ValueError("Must be a object of class Geek")
+
+    def __lshift__(self, obj):
+        print("lshift operator overloaded")
+        if isinstance(obj, Geek):
+            return self.value << obj.value
+        else:
+            raise ValueError("Must be a object of class Geek")
+
+    def __rshift__(self, obj):
+        print("rshift operator overloaded")
+        if isinstance(obj, Geek):
+            return self.value >> obj.value
+        else:
+            raise ValueError("Must be a object of class Geek")
+
+    def __invert__(self):
+        print("Invert operator overloaded")
+        return ~self.value
+
+
+# Driver's code
+if __name__ == "__main__":
+    a = BitwiseOperatorOverloading(10)
+    b = BitwiseOperatorOverloading(12)
+    print(a & b)
+    print(a | b)
+    print(a ^ b)
+    print(a << b)
+    print(a >> b)
+    print(~a)
+
+# OUTPUT
+# And operator overloaded
+# 8
+# Or operator overloaded
+# 14
+# Xor operator overloaded
+# 6
+# lshift operator overloaded
+# 40960
+# rshift operator overloaded
+# 0
+# Invert operator overloaded
+# -11
